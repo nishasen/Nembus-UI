5ec2c508d48e4e4e668baef76448dd5c4a31e6a2
nishasen <43262505+nishasen@users.noreply.github.com>
2022-01-31 01:02:13 +0530

    Badge completed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 /* Alert with close button */
 
-// const alertBox = document.querySelector("#alert-close")
-// const alertClose = document.querySelector("#alert-close-button")
+const alertBox = document.querySelector("#alert-close")
+const alertClose = document.querySelector("#alert-close-button")
 
-// alertClose.addEventListener("click", () => {
-//     alertBox.style.display = "none";
-// });
+alertClose.addEventListener("click", () => {
+    alertBox.style.display = "none";
+});
