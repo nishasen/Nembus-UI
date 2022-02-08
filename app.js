a978c037a243b7230867cb2e0047d1f1450b5e63
nishasen <43262505+nishasen@users.noreply.github.com>
2022-02-08 18:33:05 +0530

    Made some minor changes

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const sideNavOpen = document.querySelector("#open-side-nav")
 const sideNavClose = document.querySelector("#close-side-nav")
 const sideNav = document.querySelector("#side-nav")
+const componentBody = document.querySelector("#comp-body")
 const slider = document.querySelector("#slider")
 const valueOfSlider = document.querySelector("#slider-value")
 const stepSlider = document.querySelector("#step-slider")
 const valueOfStepSlider = document.querySelector("#step-slider-value")
 
-valueOfSlider.innerHTML = slider.value;
-valueOfStepSlider.innerHTML = stepSlider.value;
-
 sideNavOpen.addEventListener("click", () => {
-    sideNav.style.width="13rem";
+    sideNav.style.width = "13rem";
 })
 
 sideNavClose.addEventListener("click", () => {
-    sideNav.style.width="0";
+    sideNav.style.width = "0";
 })
+
+valueOfSlider.innerHTML = slider.value;
+valueOfStepSlider.innerHTML = stepSlider.value;
 
 slider.addEventListener("input", ()=>{
     valueOfSlider.innerHTML = slider.value;
