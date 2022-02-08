c1d5a092b4eaa7cb3dd9a509aac3f5661e783add
nishasen <43262505+nishasen@users.noreply.github.com>
2022-02-08 16:01:43 +0530

    Added example code

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const sideNavOpen = document.querySelector("#open-side-nav")
 const sideNavClose = document.querySelector("#close-side-nav")
 const sideNav = document.querySelector("#side-nav")
-const body = document.querySelector("#comp-body")
+const slider = document.querySelector("#slider")
+const valueOfSlider = document.querySelector("#slider-value")
+const stepSlider = document.querySelector("#step-slider")
+const valueOfStepSlider = document.querySelector("#step-slider-value")
+
+valueOfSlider.innerHTML = slider.value;
+valueOfStepSlider.innerHTML = stepSlider.value;
 
 sideNavOpen.addEventListener("click", () => {
     sideNav.style.width="13rem";
@@ -10,3 +16,13 @@ sideNavOpen.addEventListener("click", () => {
 sideNavClose.addEventListener("click", () => {
     sideNav.style.width="0";
 })
+
+slider.addEventListener("input", ()=>{
+    valueOfSlider.innerHTML = slider.value;
+})
+
+stepSlider.addEventListener("input", ()=>{
+    valueOfStepSlider.innerHTML = stepSlider.value;
+})
+
+
