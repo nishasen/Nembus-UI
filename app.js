f015105b5cac2d068b485171b59f8683250b5743
nishasen <43262505+nishasen@users.noreply.github.com>
2022-02-08 11:44:03 +0530

    Updated components
    Added new grid, rating and slider component, for that updated the other components

--- app.js
+++ app.js
@@ -5,14 +5,8 @@ const body = document.querySelector("#comp-body")
 
 sideNavOpen.addEventListener("click", () => {
     sideNav.style.width="13rem";
-    // body.style.paddingLeft="14rem";
-    // body.style.paddingRight="2rem";
-
 })
 
 sideNavClose.addEventListener("click", () => {
     sideNav.style.width="0";
-    // body.style.paddingLeft="1rem";
-    // body.style.paddingRight="2rem";
-    // body.style.margin="0 auto";
 })
